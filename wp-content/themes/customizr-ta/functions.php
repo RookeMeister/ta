82d003e6c52f669ebfae348b10b77b3ed74ecda7
Peter Rooke <peterwrooke@gmail.com>
2014-01-11 13:29:23 +1100

    working version of custom Admin WIP button

--- wp-content/themes/customizr-ta/functions.php
+++ wp-content/themes/customizr-ta/functions.php
@@ -30,18 +30,21 @@ function format_dps_title( $output, $atts, $image, $title, $date, $excerpt, $inn
 	return $output;
 }
 
-function custom_button_example($wp_admin_bar){
-$args = array(
-'id' => 'custom-button',
-'title' => 'Custom Button',
-'href' => 'http://example.com/',
-'meta' => array(
-'class' => 'custom-button-class'
-)
-);
-$wp_admin_bar->add_node($args);
+// Peter Rooke
+//----------------------------------------------------------
+// Add action and function to add WIP button to Admin bar
+//----------------------------------------------------------
+add_action('admin_bar_menu', 'wip_button', 95);
+function wip_button($wp_admin_bar){
+	$args = array(
+		'id' => 'wip-button',
+		'title' => 'Work In Progress',
+		'href' => 'http://www.rookemeister.com/ta/?page_id=274',
+		'meta' => array(
+			'class' => 'custom-button-class'
+		)
+	);
+	$wp_admin_bar->add_node($args);
 }
-
-add_action('admin_bar_menu', 'custom_button_example', 50);
 
 ?>
