8632ce278f6f05de36fed2b06f271f2bd6e80cb6
Peter Rooke <peterwrooke@gmail.com>
2014-01-25 17:54:28 +1100

    gateway container and blank template completed

--- wp-content/themes/customizr-ta/background-only.php
+++ wp-content/themes/customizr-ta/background-only.php
@@ -6,12 +6,7 @@
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<style type="text/css">
-body { background-color: #cccccc; background-repeat: repeat-y; background-position: top center; background-attachment: scroll; }
-#page-content { width: 640px; margin: 20px auto; }
-#page-content p { font-size: 1em; font-family: Georgia; line-height: 1.5em; text-align: justify; }
-</style>
+
 <title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
 <?php wp_head(); ?>
 </head>
